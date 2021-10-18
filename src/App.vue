5c0ed9d3be3e17c6fcc4b9dce550951ce88afa78
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-10-18 19:06:23 +0300

    Initial project structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar class="px-5" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">My Vue Shoes</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ms-3">
+          <b-nav-item href="#" active>Products</b-nav-item>
+          <b-nav-item href="#">Prices</b-nav-item>
+          <b-nav-item href="#">About Us</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ms-auto"> </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <main class="p-5">
+      <b-row class="mx-auto d-flex justify-content-center gap-5">
+        <Card
+          v-for="item in shoesInfo"
+          :data="item"
+          :key="item.id"
+        />
+      </b-row>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
+import blue from "./assets/nike-blue.jpg";
+import pink from "./assets/nike-pink.jpg";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      shoesInfo: [
+        {
+          id: 1,
+          photo: blue,
+          category: "Men Sports Shoe",
+          title: "Nike Epic React Flyknit Sky Blue",
+          summary: "Best for running and having fun in the nature",
+          sizes: [36, 37, 38, 39, 40, 41, 42],
+          price: 100,
+          star: 4,
+        },
+        {
+          id: 2,
+          photo: pink,
+          category: "Women Sports Shoe",
+          title: "Nike Epic React Flyknit Sky Pink",
+          summary: "Best for running and having fun in the nature",
+          sizes: [36, 37, 38, 39, 40, 41, 42],
+          price: 100,
+          star: 3,
+        },
+        {
+          id: 3,
+          photo: blue,
+          category: "Men Sports Shoe",
+          title: "Nike Epic React Flyknit Sky Blue",
+          summary: "Best for running and having fun in the nature",
+          sizes: [36, 37, 38, 39, 40, 41, 42],
+          price: 100,
+          star: 2,
+        },
+        {
+          id: 4,
+          photo: pink,
+          category: "Women Sports Shoe",
+          title: "Nike Epic React Flyknit Sky Pink",
+          summary: "Best for running and having fun in the nature",
+          sizes: [36, 37, 38, 39, 40, 41, 42],
+          price: 100,
+          star: 5,
+        },
+      ],
+    };
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-card
+    :title="data.title"
+    :sub-title="data.category"
+    :img-alt="data.category"
+    style="max-width: 25rem;"
+  >
+    <b-card-img class="mb-3" :src="data.photo"></b-card-img>
+    <b-card-text>
+      {{ data.summary }}
+    </b-card-text>
+    <div>
+      <div class="d-flex align-items-center justify-content-evenly">
+        <p class="m-0 h4">{{ data.price }}$</p>
+        <b-button variant="secondary">Add to Cart</b-button>
+      </div>
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data() {
+    return {
+      selected: 36,
+    };
+  },
+  props: {
+    data: Object,
+  },
+};
+</script>
+
+<style scoped></style>
